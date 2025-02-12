6aae0583a122dc509ce8642b2bc47a036bdd0e4f
AnnaBushuevaV <anna.gulyaeva.v.10@yandex.ru>
2025-02-12 18:54:48 +0500

    commit from VS

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -1,6 +1,14 @@
 /*
-
 Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), и выводит сумму равную 
 n + nn + nnn, где n не перемножаются, а конкатенируются
-
 */
+ let a = 5;
+ let modifiedTypea = Number(a)
+ console.log(modifiedTypea)
+ let b = String(a) + a;
+ let modifiedTypeb = Number(b)
+ console.log(modifiedTypeb)
+ let c = String(a) + a + a;
+ let modifiedTypec = Number(c)
+ console.log(modifiedTypec)
+ console.log (modifiedTypea + modifiedTypeb + modifiedTypec)
