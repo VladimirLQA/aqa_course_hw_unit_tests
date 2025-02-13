68c1ad2372e5a92502a4b54e4c5befcb936165d7
superpusya <xxx.xxx-xx2018@ya.ru>
2025-02-13 19:13:58 +0400

    Сделала второе задание в hw-3/task-2.js

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,18 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+
+let n = 1; 
+
+if (n >= 1 && n <= 9) {
+    n = String(n);
+    let nn = String(n) + String(n); // "nn" 
+    let nnn = String(n) + String(n) + String(n); // "nnn"
+
+    let result = n + nn + nnn;
+
+    console.log("Результат:", result);
+} else {
+    console.log("Пожалуйста, введите корректное число от 1 до 9.");
+}
+
