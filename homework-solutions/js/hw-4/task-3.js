943b9323da1fa2ced9ad8dbcb7026eef33f8699b
JosieVi <o.s.marushkina@gmail.com>
2025-02-16 01:34:02 +0300

    Completed hw-4; add task-1.js & task-2.js & task-3.js

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,27 @@
   преобразовываясь в number
 
 */
+
+let minAge = 18;
+let maxAge = 60;  
+let age = '34';
+if (typeof age === 'string') {
+  age = +age;
+}
+else if (typeof age !== 'number') {
+  console.log('Incorrect data type');
+}
+if (age) {
+  if (age < minAge) {
+    console.log(`You don't have access cause your age is ${age}. It's less then ${minAge}`);
+  }
+  else if (minAge <= age && age <= maxAge) {
+    console.log('Welcome!');
+  }
+  else if (age > maxAge) {
+    console.log('Keep calm and look Culture channel');
+  }
+}
+else {
+  console.log('Technical work');
+}
