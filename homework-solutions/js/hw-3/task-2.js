9c493d813064c47e11a0111ac7b064fe5e695e90
Pavel <northdy12@gmail.com>
2025-02-16 13:21:02 +0100

    adds hw-3 tasks 1 and 2

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -1,6 +1,28 @@
 /*
 
-Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), и выводит сумму равную 
+Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), и выводит сумму равную
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question("Введите целое положительное число от 1 до 9: ", (number) => {
+
+    const num = Number(number)
+
+    if (number > 0 && number <= 9) {
+        const numberOne = Number(number)
+        const numberTwo = String(number) + String(number)
+        const numberThree = String(number) + String(number) + String(number)
+        console.log("Результат:", numberOne + Number(numberTwo) + Number(numberThree))
+    }
+
+    else if (typeof number !== 'number') {
+        console.log("Неверный ввод. Принимаются только целые числа от 1 до 9.");
+    }
+    readline.close();
+});
