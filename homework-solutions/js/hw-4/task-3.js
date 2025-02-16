3400e16b1be9dbc89ecaf623ffe82d3f7d04dd9a
superpusya <xxx.xxx-xx2018@ya.ru>
2025-02-16 19:47:14 +0400

    третье задание из четвертого дз в файле hw-4/task-3.js

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,23 @@
   преобразовываясь в number
 
 */
+
+let minAge = 18;
+let maxAge = 60;
+let age = '2';
+
+if (typeof age === 'string' && /^\d+$/.test(age)) {
+    age = Number(age);
+}
+
+if (typeof age !== 'number') {
+    console.log("Incorrect data type");
+} else if (age < minAge) {
+    console.log("You don't have access cause your age is " + age + " It's less than " + minAge);
+} else if (age >= minAge && age < maxAge) {
+    console.log("Welcome!");
+} else if (age > maxAge) {
+    console.log("Keep calm and look Culture channel.");
+} else {
+    console.log("Technical work");
+}
