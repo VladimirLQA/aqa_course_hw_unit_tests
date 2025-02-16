19964886cf8590da94b6074a36e2ceb10d38d0bb
Tikimilk <pervozvanskaya123@gmail.com>
2025-02-16 22:34:04 +0300

    HM_4

--- homework-solutions/js/hw-4/task-1.js
+++ homework-solutions/js/hw-4/task-1.js
@@ -2,4 +2,13 @@
 
  - Создайте переменную salary со значением 1000
   - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000, и значение "junior" - если меньше
+  
   */
+let salary = 1001;
+let grade;
+if (salary>= 1000){
+    grade="midle";
+} else {
+    grade="junior";
+}
+console.log(grade);
