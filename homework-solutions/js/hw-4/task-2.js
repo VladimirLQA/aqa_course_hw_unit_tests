9962733261360b404da3a2865237d050ce45806a
JosieVi <o.s.marushkina@gmail.com>
2025-02-18 00:28:25 +0300

    Fixed task-2.js and task-3.js in HW-4 based on reviewer feedback

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -15,12 +15,33 @@
 
   */
 
+// 1ый вариант (с использованием && в условиях)
+
+// let minAge = 18;
+// let maxAge = 60;
+// let age = 40;
+// if (typeof age !== 'number' || isNaN(age)) {
+//   console.log('Incorrect data type');
+// }
+// else if (age >= 0) {
+//   age < minAge && console.log(`You don't have access cause your age is ${age}. It's less then ${minAge}`);
+//   minAge <= age && age <= maxAge && console.log('Welcome!');
+//   age > maxAge && console.log('Keep calm and look Culture channel');
+// }
+// else {
+//   console.log('Technical work');
+// }
+ 
+// 2ой вариант (без использования && в условиях)
+
 let minAge = 18;
 let maxAge = 60;
-let age;
-typeof age !== 'number' && console.log('Incorrect data type');
-if (age) {
-  if (age < minAge) {
+let age = 0;
+if (typeof age !== 'number' || isNaN(age)) {
+  console.log('Incorrect data type');
+}
+else {
+  if (age >= 0 && age < minAge) {
     console.log(`You don't have access cause your age is ${age}. It's less then ${minAge}`);
   }
   else if (minAge <= age && age <= maxAge) {
@@ -29,8 +50,7 @@ if (age) {
   else if (age > maxAge) {
     console.log('Keep calm and look Culture channel');
   }
+  else {
+    console.log('Technical work');
+  }
 }
-else {
-  console.log('Technical work');
-}
-
--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -8,16 +8,14 @@
 */
 
 let minAge = 18;
-let maxAge = 60;  
-let age = '34';
-if (typeof age === 'string') {
-  age = +age;
-}
-else if (typeof age !== 'number') {
+let maxAge = 60;
+let age = '10';
+if (typeof +age !== 'number' || isNaN(age)) {
   console.log('Incorrect data type');
 }
-if (age) {
-  if (age < minAge) {
+else {
+  age = Number(age);
+  if (age >= 0 && age < minAge) {
     console.log(`You don't have access cause your age is ${age}. It's less then ${minAge}`);
   }
   else if (minAge <= age && age <= maxAge) {
@@ -26,7 +24,10 @@ if (age) {
   else if (age > maxAge) {
     console.log('Keep calm and look Culture channel');
   }
+  else {
+    console.log('Technical work');
+  }
 }
-else {
-  console.log('Technical work');
-}
+
+  
+  
