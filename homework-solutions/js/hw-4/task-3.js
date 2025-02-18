91bc1cbefb219782ee67ae38c74ab116736c54be
Tikimilk <pervozvanskaya123@gmail.com>
2025-02-18 20:40:29 +0300

    HW_4_1

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -8,10 +8,11 @@
 */
 let minAge = 18;
 let maxAge = 60;
-let age = + "1j"; 
-if (typeof age !== 'number'){
- console.log ("Incorrect data type")
-} else if (age < minAge) {
+let age = "12"; 
+if (age && isNaN(age)){
+ console.log ("Incorrect data type")}
+ age = Number(age);
+if (age < minAge) {
 console.log("You don't have access cause your age is " + "age" + " It's less then")
 } else if (age >= minAge && age <= maxAge) {
  console.log ("Welcome  !")
