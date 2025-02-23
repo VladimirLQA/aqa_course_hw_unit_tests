4e04747d5a6e6372d334d727e983bf8592ef5d2c
Olga Marushkina <104677167+JosieVi@users.noreply.github.com>
2025-02-23 19:22:41 +0300

    Update homework-solutions/js/hw-6/task-2.mjs
    Co-authored-by: Volodymyr Lazurenko <103571439+VladimirLQA@users.noreply.github.com>

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -19,15 +19,16 @@ const competitorPizzas = ['peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawa
 const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
 const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 for (const pizzaFromMyList of myPizzasT1) {
-  for (let i = 0; i < competitorPizzas.length; i++) {
-    if (competitorPizzas[i].toLowerCase() === (pizzaFromMyList.toLowerCase())) {
+  let isMatch = false;
+  
+  for (const competitorPizza of competitorPizzas) {
+    if (competitorPizza.toLowerCase() === pizzaFromMyList.toLowerCase()) {
+      isMatch = true;
       break;
     }
-    else if (i === competitorPizzas.length - 1) {
-      resultUnique.push(pizzaFromMyList);
-      break;      
-    }
   }
+  
+  if (!isMatch) resultUnique.push(pizzaFromMyList);
 }
 console.log(!resultUnique.length ? resultNull : resultUnique);
 
