822ca5c0c9cb9a5f3fe6ec9185274fbcf6653147
JosieVi <o.s.marushkina@gmail.com>
2025-03-02 00:21:39 +0300

    Completed hw-7; add task-1.mjs & task-2.mjs & task-3.mjs

--- homework-solutions/js/hw-7/task-1.mjs
+++ homework-solutions/js/hw-7/task-1.mjs
@@ -5,18 +5,31 @@
   - Например: mergeArrays([1,2], [3,4], [5,6]) // [1,2,3,4,5,6]
   - Решить с использованием Spread operator
 */
-function mergeArrays() {
-  // Ваш код
+
+function mergeArrays(...arrays) {
+  let resultArray = [];
+  return resultArray.concat(...arrays);
 }
+console.log(mergeArrays([1,2], [3,4], [5,6]));
+
 /*
   2. Devide by _
     - Написать функцию, которая преобразует любое предложение в вот_Такой_Вот_Вид и возвращает его. 
     - Первое слово должно начинаться с буквы в нижнем регистре, у остальных -  верхнем. 
     - Пример: I am super engineer => i_Am_Super_Engineer
   */
-function devideBy(sentence) {
-  // Ваш код
+
+function devideBy(sentence = '') {
+  let resultSentence = [...sentence.trim().replace(/ +/g, '_').toLowerCase()];
+  for (let i = 0; i < resultSentence.length; i++) {
+    if (resultSentence[i] === '_') {
+      resultSentence[i+1] = resultSentence[i+1].toUpperCase();
+    }
+  }
+  return resultSentence.join('');
 }
+console.log(devideBy('I    am    an     engineer'));
+
 /*
   3. Фибаначчи
     - Напишите функцию fibonacci(n), возвращающую энное число Фибоначчи
@@ -25,8 +38,19 @@ function devideBy(sentence) {
       является суммой двух предыдущих
     - Например fibonacci(8) //21
   */
+
 function fibonacci(n) {
-  // Ваш код
+  if (n >= 2) {
+    let fibonachiArray = [0,1];
+    for (let i = 2; i <= n; i++) {
+      fibonachiArray.push(fibonachiArray[i-1]+fibonachiArray[i-2]); 
+    }
+    return fibonachiArray.at(-1);
+  }
+  else if ( n === 0 || n === 1) {
+    return n;
+  }
 }
+console.log(fibonacci(6));
 
 export { mergeArrays, fibonacci, devideBy };
--- homework-solutions/js/hw-7/task-2.mjs
+++ homework-solutions/js/hw-7/task-2.mjs
@@ -4,8 +4,20 @@
 */
 
 function isPalindrom(word) {
-  // Ваш код
+  if (typeof word === 'string') {
+    for (let i = 0; i < Math.floor(word.length/2); i++ ) {
+      if (word[i].toLowerCase() !== word.at(-1-i).toLowerCase()) {
+        return false;
+      }
+    }
+    return true;
+
+  }
+  else {
+    return false;
+  } 
 }
+console.log(isPalindrom('dfgd5dgfD'));
 
 /*
  2. findLongestWords()
@@ -14,8 +26,28 @@ function isPalindrom(word) {
  Если таких слов несколько - возвращает их все.
 */
 
-function findLongestWords(sentence) {
-  // Ваш код
+function findLongestWords(sentence = '') {
+  if (typeof sentence !== 'string' || sentence === '') {
+    return [];
+  }
+  else {
+    let sentenceInArray = sentence.replace(/ +/g,' ').split(' ');
+    let result = []; 
+    let longestWord = sentenceInArray[0].match(/[a-zA-Z]/g).join('');
+    for (let i = 0; i < sentenceInArray.length; i++) {
+      if (sentenceInArray[i].match(/[a-zA-Z]/g).join('').length === longestWord.length) {
+        result.push(sentenceInArray[i]);
+      }
+      else if ((sentenceInArray[i].match(/[a-zA-Z]/g).join('').length > longestWord.length)) {
+        result.splice(0);
+        result.push(sentenceInArray[i]);
+        longestWord = sentenceInArray[i].match(/[a-zA-Z]/g).join('');
+      }
+    }
+      return result;
+  }
 }
+console.log(findLongestWords('It\'s    crazy, this\'s a party'));
+console.log(findLongestWords(123));
 
 export { isPalindrom, findLongestWords };
