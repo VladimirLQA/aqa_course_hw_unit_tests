b8b0e3652e409dd2219d0f27cf45cffaf2883924
superpusya <xxx.xxx-xx2018@ya.ru>
2025-03-01 21:57:57 +0400

    сделала первое задание из седьмого дз в файле hw-7/task-1.js

--- homework-solutions/js/hw-7/task-1.mjs
+++ homework-solutions/js/hw-7/task-1.mjs
@@ -5,18 +5,51 @@
   - Например: mergeArrays([1,2], [3,4], [5,6]) // [1,2,3,4,5,6]
   - Решить с использованием Spread operator
 */
-function mergeArrays() {
-  // Ваш код
+
+function mergeArrays(...arrays) {
+  return [...arrays.flat()];
 }
+
 /*
   2. Devide by _
     - Написать функцию, которая преобразует любое предложение в вот_Такой_Вот_Вид и возвращает его. 
     - Первое слово должно начинаться с буквы в нижнем регистре, у остальных -  верхнем. 
     - Пример: I am super engineer => i_Am_Super_Engineer
   */
-function devideBy(sentence) {
-  // Ваш код
+function normalizeSpaces(sentence) {
+  let words = [];
+  let word = ''; //для временного хранения текущего слова
+  for (let i = 0; i < sentence.length; i++) {
+    //проходим по каждому символу в предложении
+    if (sentence[i] === ' ') {
+      if (word !== '') {
+        words.push(word);
+        word = '';
+      }
+    } else {
+      word += sentence[i];
+    }
+  }
+  if (word !== '') {
+    words.push(word);
+  }
+  return words.join(' ');
 }
+
+function devideBy(sentence) {
+  let normalizedSentence = normalizeSpaces(sentence);
+  let words = normalizedSentence.toLowerCase().split(' ');
+  let result = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    result += '_' + words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+
+  return result;
+}
+
+console.log(devideBy('I    am    an     engineer'));
+
 /*
   3. Фибаначчи
     - Напишите функцию fibonacci(n), возвращающую энное число Фибоначчи
@@ -25,8 +58,13 @@ function devideBy(sentence) {
       является суммой двух предыдущих
     - Например fibonacci(8) //21
   */
+
 function fibonacci(n) {
-  // Ваш код
+  let fib = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  return fib[n];
 }
 
 export { mergeArrays, fibonacci, devideBy };
