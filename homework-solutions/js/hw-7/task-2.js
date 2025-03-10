0d0fabd1b9d187503cf55ada8681e155453fddc9
AvramenkoDmitry <Avramenkodmytro1998@gmail.com>
2025-03-10 17:39:12 +0100

    homework 7

--- homework-solutions/js/hw-7/task-2.js
+++ homework-solutions/js/hw-7/task-2.js
@@ -3,8 +3,24 @@
  Написать функцию, которая принимает на вход слово и проверяет, является ли это слово палиндромом
 */
 
-function isPalindrom(word) {
+/*function isPalindrom(word) {
+  const newWord = word.split('').reverse().join('');
+  return word === newWord;
   // Ваш код
+}
+*/
+function isPalindrom(word) {
+  // Проверка, что word является строкой
+  if (typeof word !== 'string') {
+    return false; // Возвращаем false, если вход не строка
+  }
+
+  // Приводим строку к нижнему регистру, чтобы игнорировать регистр при проверке
+  const lowerWord = word.toLowerCase();
+
+  // Преобразуем слово в обратный порядок и проверяем на равенство
+  const newWord = lowerWord.split('').reverse().join('');
+  return lowerWord === newWord;
 }
 
 /*
@@ -14,8 +30,55 @@ function isPalindrom(word) {
  Если таких слов несколько - возвращает их все.
 */
 
+/*function findLongestWords(sentence) {
+  
+  const words = sentence.split(' '); //Разбиваем предложение на массив слов
+  let maxLength = 0;
+  let longestWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+      longestWords = [words[i]]; // Обновляем массив, если нашли слово длиннее
+    } else if (words[i].length === maxLength) {
+      longestWords.push(words[i]); // Добавляем, если длина совпадает с максимальной
+    }
+  }
+  return longestWords;
+  
+}
+ */
 function findLongestWords(sentence) {
-  // Ваш код
+  // Проверяем, что передан строковый параметр
+  if (typeof sentence !== 'string') {
+    return []; // Вместо ошибки возвращаем пустой массив
+  }
+
+  // Убираем лишние пробелы в начале и в конце строки, а затем разбиваем строку на массив слов
+  const words = sentence.trim().split(' ');
+
+  // Если строка пуста, возвращаем пустой массив
+  if (words.length === 1 && words[0] === '') {
+    return [];
+  }
+
+  let maxLength = 0;
+  let longestWords = [];
+
+  // Проходим по каждому слову в массиве
+  for (let i = 0; i < words.length; i++) {
+    // Если текущая длина слова больше максимальной, обновляем maxLength и longestWords
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+      longestWords = [words[i]]; // Создаем новый массив с единственным длинным словом
+    }
+    // Если длина слова равна максимальной, добавляем его в массив longestWords
+    else if (words[i].length === maxLength) {
+      longestWords.push(words[i]);
+    }
+  }
+
+  return longestWords;
 }
 
 export { isPalindrom, findLongestWords };
